7718e45934f48480778cd9adca0ec9a5eb5e2927
Nicolas Schurando <nicolas@sitronlabs.com>
2023-07-15 16:04:48 +0200

    Reduced ram footprint.

--- src/main.cpp
+++ src/main.cpp
@@ -15,16 +15,15 @@
 /* Working variables */
 static tic m_tic;
 
-/* Wireless messages */
-static MyMessage m_message_info_serial(0, V_TEXT);
-static MyMessage m_message_power_kwh(1, V_KWH);
-static MyMessage m_message_power_watt(2, V_WATT);
-static MyMessage m_message_phase1_voltage(3, V_VOLTAGE);
-static MyMessage m_message_phase1_current(3, V_CURRENT);
-static MyMessage m_message_phase2_voltage(4, V_VOLTAGE);
-static MyMessage m_message_phase2_current(4, V_CURRENT);
-static MyMessage m_message_phase3_voltage(5, V_VOLTAGE);
-static MyMessage m_message_phase3_current(5, V_CURRENT);
+/* List of virtual sensors */
+enum {
+    SENSOR_0_SERIAL_NUMBER,       // V_TEXT
+    SENSOR_1_INDEX_BASE,          // V_KWH
+    SENSOR_2_POWER_APPARENT,      // V_WATT
+    SENSOR_3_MULTIMETER_PHASE_1,  // V_VOLTAGE and V_CURRENT
+    SENSOR_4_MULTIMETER_PHASE_2,  // V_VOLTAGE and V_CURRENT
+    SENSOR_5_MULTIMETER_PHASE_3,  // V_VOLTAGE and V_CURRENT
+};
 
 /**
  * Setup function.
@@ -77,13 +76,13 @@ void setup(void) {
 void presentation(void) {
     int res = 1;
     do {
-        res &= sendSketchInfo("SLHA00011 Tic", "0.1.1");
-        res &= present(0, S_INFO, "Numéro de Série");       // V_TEXT (ADCO, ADSC)
-        res &= present(1, S_POWER, "Index Base");           // V_KWH (BASE)
-        res &= present(2, S_POWER, "Puissance Apparente");  // V_WATT (PAPP)
-        res &= present(3, S_MULTIMETER, "Phase 1");         // V_VOLTAGE (URMS1) and V_CURRENT (IINST, IINST1, IRMS1)
-        res &= present(4, S_MULTIMETER, "Phase 2");         // V_VOLTAGE (URMS2) and V_CURRENT (IINST2, IRMS2)
-        res &= present(5, S_MULTIMETER, "Phase 3");         // V_VOLTAGE (URMS3) and V_CURRENT (IINST3, IRMS3)
+        res &= sendSketchInfo(F("SLHA00011 Linky"), F("0.1.1"));
+        res &= present(SENSOR_0_SERIAL_NUMBER, S_INFO, F("Numéro de Série"));        // V_TEXT (ADCO, ADSC)
+        res &= present(SENSOR_1_INDEX_BASE, S_POWER, F("Index Base"));               // V_KWH (BASE)
+        res &= present(SENSOR_2_POWER_APPARENT, S_POWER, F("Puissance Apparente"));  // V_WATT (PAPP)
+        res &= present(SENSOR_3_MULTIMETER_PHASE_1, S_MULTIMETER, F("Phase 1"));     // V_VOLTAGE (URMS1) and V_CURRENT (IINST, IINST1, IRMS1)
+        res &= present(SENSOR_4_MULTIMETER_PHASE_2, S_MULTIMETER, F("Phase 2"));     // V_VOLTAGE (URMS2) and V_CURRENT (IINST2, IRMS2)
+        res &= present(SENSOR_5_MULTIMETER_PHASE_3, S_MULTIMETER, F("Phase 3"));     // V_VOLTAGE (URMS3) and V_CURRENT (IINST3, IRMS3)
     } while (res == 0);
 }
 
@@ -176,19 +175,18 @@ void loop(void) {
         tic_valid = true;
 
         /* Serial number */
-        if (strcmp(dataset.name, "ADCO") == 0 || strcmp(dataset.name, "ADSC") == 0) {
+        if (strcmp_P(dataset.name, PSTR("ADCO")) == 0 || strcmp_P(dataset.name, PSTR("ADSC")) == 0) {
             static bool initial_sent = false;
             if (initial_sent == false) {
-                char tic_serial[12 + 1] = "";
-                strncpy(tic_serial, dataset.data, 12);
-                if (send(m_message_info_serial.set(tic_serial)) == true) {
+                MyMessage message(SENSOR_0_SERIAL_NUMBER, V_TEXT);
+                if (send(message.set(dataset.data)) == true) {
                     initial_sent = true;
                 }
             }
         }
 
         /* Index for base */
-        else if (strcmp(dataset.name, "BASE") == 0) {
+        else if (strcmp_P(dataset.name, PSTR("BASE")) == 0) {
             static bool initial_sent = false;
             static uint32_t base_wh_last = 0;
             uint32_t base_wh = 0;
@@ -201,7 +199,8 @@ void loop(void) {
                 }
             }
             if (base_wh > base_wh_last || initial_sent == false) {
-                if (send(m_message_power_kwh.set(base_wh / 1000.0, 3)) == true) {
+                MyMessage message(SENSOR_1_INDEX_BASE, V_KWH);
+                if (send(message.set(base_wh / 1000.0, 3)) == true) {
                     initial_sent = true;
                     base_wh_last = base_wh;
                 }
@@ -209,7 +208,7 @@ void loop(void) {
         }
 
         /* Puissance apparente */
-        else if (strcmp(dataset.name, "PAPP") == 0) {
+        else if (strcmp_P(dataset.name, PSTR("PAPP")) == 0) {
             static bool initial_sent = false;
             static uint32_t power_va_last = 0;
             uint32_t power_va = 0;
@@ -222,7 +221,8 @@ void loop(void) {
                 }
             }
             if (power_va != power_va_last || initial_sent == false) {
-                if (send(m_message_power_watt.set(power_va)) == true) {
+                MyMessage message(SENSOR_2_POWER_APPARENT, V_WATT);
+                if (send(message.set(power_va)) == true) {
                     initial_sent = true;
                     power_va_last = power_va;
                 }
@@ -230,7 +230,7 @@ void loop(void) {
         }
 
         /* Intensité Phase 1 */
-        else if (strcmp(dataset.name, "IINST") == 0 || strcmp(dataset.name, "IINST1") == 0 || strcmp(dataset.name, "IRMS1") == 0) {
+        else if (strcmp_P(dataset.name, PSTR("IINST")) == 0 || strcmp_P(dataset.name, PSTR("IINST1")) == 0 || strcmp_P(dataset.name, PSTR("IRMS1")) == 0) {
             static bool initial_sent = false;
             static uint32_t current_a_last = 0;
             uint32_t current_a = 0;
@@ -243,7 +243,8 @@ void loop(void) {
                 }
             }
             if (current_a != current_a_last || initial_sent == false) {
-                if (send(m_message_phase1_current.set(current_a)) == true) {
+                MyMessage message(SENSOR_3_MULTIMETER_PHASE_1, V_CURRENT);
+                if (send(message.set(current_a)) == true) {
                     initial_sent = true;
                     current_a_last = current_a;
                 }
@@ -251,7 +252,7 @@ void loop(void) {
         }
 
         /* Tension Phase 1 */
-        else if (strcmp(dataset.name, "URMS1") == 0) {
+        else if (strcmp_P(dataset.name, PSTR("URMS1")) == 0) {
             static bool initial_sent = false;
             static uint16_t voltage_v_last = 0;
             uint16_t voltage_a = 0;
@@ -264,7 +265,8 @@ void loop(void) {
                 }
             }
             if (voltage_a != voltage_v_last || initial_sent == false) {
-                if (send(m_message_phase1_voltage.set(voltage_a)) == true) {
+                MyMessage message(SENSOR_3_MULTIMETER_PHASE_1, V_VOLTAGE);
+                if (send(message.set(voltage_a)) == true) {
                     initial_sent = true;
                     voltage_v_last = voltage_a;
                 }
@@ -272,7 +274,7 @@ void loop(void) {
         }
 
         /* Intensité Phase 2 */
-        else if (strcmp(dataset.name, "IINST2") == 0 || strcmp(dataset.name, "IRMS2") == 0) {
+        else if (strcmp_P(dataset.name, PSTR("IINST2")) == 0 || strcmp_P(dataset.name, PSTR("IRMS2")) == 0) {
             static bool initial_sent = false;
             static uint32_t current_a_last = 0;
             uint32_t current_a = 0;
@@ -285,7 +287,8 @@ void loop(void) {
                 }
             }
             if (current_a != current_a_last || initial_sent == false) {
-                if (send(m_message_phase2_current.set(current_a)) == true) {
+                MyMessage message(SENSOR_4_MULTIMETER_PHASE_2, V_CURRENT);
+                if (send(message.set(current_a)) == true) {
                     initial_sent = true;
                     current_a_last = current_a;
                 }
@@ -293,7 +296,7 @@ void loop(void) {
         }
 
         /* Tension Phase 2 */
-        else if (strcmp(dataset.name, "URMS2") == 0) {
+        else if (strcmp_P(dataset.name, PSTR("URMS2")) == 0) {
             static bool initial_sent = false;
             static uint16_t voltage_v_last = 0;
             uint16_t voltage_a = 0;
@@ -306,7 +309,8 @@ void loop(void) {
                 }
             }
             if (voltage_a != voltage_v_last || initial_sent == false) {
-                if (send(m_message_phase2_voltage.set(voltage_a)) == true) {
+                MyMessage message(SENSOR_4_MULTIMETER_PHASE_2, V_VOLTAGE);
+                if (send(message.set(voltage_a)) == true) {
                     initial_sent = true;
                     voltage_v_last = voltage_a;
                 }
@@ -314,7 +318,7 @@ void loop(void) {
         }
 
         /* Intensité Phase 3 */
-        else if (strcmp(dataset.name, "IINST3") == 0 || strcmp(dataset.name, "IRMS3") == 0) {
+        else if (strcmp_P(dataset.name, PSTR("IINST3")) == 0 || strcmp_P(dataset.name, PSTR("IRMS3")) == 0) {
             static bool initial_sent = false;
             static uint32_t current_a_last = 0;
             uint32_t current_a = 0;
@@ -327,7 +331,8 @@ void loop(void) {
                 }
             }
             if (current_a != current_a_last || initial_sent == false) {
-                if (send(m_message_phase3_current.set(current_a)) == true) {
+                MyMessage message(SENSOR_5_MULTIMETER_PHASE_3, V_CURRENT);
+                if (send(message.set(current_a)) == true) {
                     initial_sent = true;
                     current_a_last = current_a;
                 }
@@ -335,7 +340,7 @@ void loop(void) {
         }
 
         /* Tension Phase 3 */
-        else if (strcmp(dataset.name, "URMS3") == 0) {
+        else if (strcmp_P(dataset.name, PSTR("URMS3")) == 0) {
             static bool initial_sent = false;
             static uint16_t voltage_v_last = 0;
             uint16_t voltage_a = 0;
@@ -348,7 +353,8 @@ void loop(void) {
                 }
             }
             if (voltage_a != voltage_v_last || initial_sent == false) {
-                if (send(m_message_phase3_voltage.set(voltage_a)) == true) {
+                MyMessage message(SENSOR_5_MULTIMETER_PHASE_3, V_VOLTAGE);
+                if (send(message.set(voltage_a)) == true) {
                     initial_sent = true;
                     voltage_v_last = voltage_a;
                 }
